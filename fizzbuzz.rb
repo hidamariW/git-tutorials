73c215556bcd2c0655c52e780455cf0aeb332cc0
wakazuki <j1840@matsue-ct.ac.jp>
2021-10-27 11:26:33 +0900

    fizzbuzz-first

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+#!/bin/ruby
+
+i = 1
+while(i<=100)do
+  if (i % 3 == 0 && i % 5 == 0)
+    print "FizzBuzz\n"
+  elsif (i % 3 == 0)
+    print "Fizz\n"
+  elsif (i % 5 == 0)
+    print "Buzz\n"
+  end
+end
+
